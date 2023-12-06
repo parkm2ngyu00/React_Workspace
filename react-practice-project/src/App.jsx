c37b58763ff5ac39a7c0ec540b41d0bf96204ad4
park min kyu <88785472+parkmingue00@users.noreply.github.com>
2023-12-06 20:36:52 +0900

    리액트 상태변화 적용 프로젝트 추가

---
+++ react-practice-project/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+import Header from "./components/Header";
+import UserInput from "./Components/UesrInput";
+import Results from "./Components/Result";
+
+function App() {
+	const [userInput, setUserInput] = useState({
+		initialInvestment: 10000,
+		annualInvestment: 1200,
+		expectedReturn: 6,
+		duration: 10,
+	});
+
+	const inputIsValid = userInput.duration >= 1;
+
+	function handleChange(inputIdentifier, newValue) {
+		setUserInput((prevUserInput) => {
+			return {
+				...prevUserInput,
+				[inputIdentifier]: +newValue,
+			};
+		});
+	}
+
+	return (
+		<>
+			<Header />
+			<UserInput userInput={userInput} onChangeInput={handleChange} />
+			{!inputIsValid && (
+				<p className="center">Please enter a duration greater than zero.</p>
+			)}
+			{inputIsValid && <Results userInput={userInput} />}
+		</>
+	);
+}
+
+export default App;
