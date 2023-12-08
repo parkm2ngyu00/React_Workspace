3f24e1bdf468fef6cf99579e74ea294a61f4e9ed
park min kyu <88785472+parkmingue00@users.noreply.github.com>
2023-12-08 23:08:44 +0900

    리액트 useRef, portal 관련 공부 코드 추가

---
+++ practice-refs-portals/src/components/Player.jsx
@@ -0,0 +1,22 @@
+import { useState, useRef } from "react";
+
+export default function Player() {
+	const input = useRef();
+
+	const [playerName, setPlayerName] = useState("");
+
+	function handleClick() {
+		setPlayerName(input.current.value);
+		input.current.value = "";
+	}
+
+	return (
+		<section id="player">
+			<h2>Welcome {playerName ?? "unknown entity"}</h2>
+			<p>
+				<input ref={input} type="text" />
+				<button onClick={handleClick}>Set Name</button>
+			</p>
+		</section>
+	);
+}
